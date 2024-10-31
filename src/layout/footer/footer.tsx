917ac5d6445918cc39fe1c5578e2484113734432
syoG2 <syo53594f@gmail.com>
2024-11-01 00:56:00 +0900

    横幅の修正

--- src/layout/footer/footer.tsx
+++ src/layout/footer/footer.tsx
@@ -1,17 +1,16 @@
 import layoutStyles from '@/layout/layout.module.css'
-import Link from 'next/link'
-import styles from './footer.module.css'
 
 export default function Footer() {
     return (
         <footer className={layoutStyles.footer}>
-            <div>
+            <div>footer</div>
+            {/* <div>
                 <Link className={styles.logo} href="/">
                     <img className={styles.logoImg} src="/mu.svg" alt="logo" />
                     <div className={styles.logoText}>mumemo</div>
                 </Link>
                 <small className={styles.copyright}>&copy; 2024 syo </small>
-            </div>
+            </div> */}
         </footer>
     )
 }
--- src/layout/header/header.tsx
+++ src/layout/header/header.tsx
@@ -1,14 +1,13 @@
 import layoutStyles from '@/layout/layout.module.css'
-import Link from 'next/link'
-import styles from './header.module.css'
 
 export default function Header() {
     return (
         <header className={layoutStyles.header}>
-            <Link className={styles.logo} href="/">
+            <div>header</div>
+            {/* <Link className={styles.logo} href="/">
                 <img className={styles.logoImg} src="/mu.svg" alt="logo" />
                 <div className={styles.logoText}>mumemo</div>
-            </Link>
+            </Link> */}
         </header>
     )
 }
