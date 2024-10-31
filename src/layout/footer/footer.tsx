d1d9a9ff885e8bababd980fb60e3895cc917bcfc
syoG2 <syo53594f@gmail.com>
2024-11-01 01:04:11 +0900

    レスポンシブ対応の調整

--- src/layout/footer/footer.tsx
+++ src/layout/footer/footer.tsx
@@ -1,16 +1,17 @@
 import layoutStyles from '@/layout/layout.module.css'
+import Link from 'next/link'
+import styles from './footer.module.css'
 
 export default function Footer() {
     return (
         <footer className={layoutStyles.footer}>
-            <div>footer</div>
-            {/* <div>
+            <div>
                 <Link className={styles.logo} href="/">
                     <img className={styles.logoImg} src="/mu.svg" alt="logo" />
                     <div className={styles.logoText}>mumemo</div>
                 </Link>
                 <small className={styles.copyright}>&copy; 2024 syo </small>
-            </div> */}
+            </div>
         </footer>
     )
 }
--- src/layout/header/header.tsx
+++ src/layout/header/header.tsx
@@ -1,13 +1,14 @@
 import layoutStyles from '@/layout/layout.module.css'
+import Link from 'next/link'
+import styles from './header.module.css'
 
 export default function Header() {
     return (
         <header className={layoutStyles.header}>
-            <div>header</div>
-            {/* <Link className={styles.logo} href="/">
+            <Link className={styles.logo} href="/">
                 <img className={styles.logoImg} src="/mu.svg" alt="logo" />
                 <div className={styles.logoText}>mumemo</div>
-            </Link> */}
+            </Link>
         </header>
     )
 }
