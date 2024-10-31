4009a6a09b15bb57a3f002a7159ba73a436e09b2
syoG2 <syo53594f@gmail.com>
2024-11-01 00:40:18 +0900

    header、footerを設定

--- src/layout/header/header.tsx
+++ src/layout/header/header.tsx
@@ -1,8 +1,14 @@
 import layoutStyles from '@/layout/layout.module.css'
+import Link from 'next/link'
+import styles from './header.module.css'
+
 export default function Header() {
     return (
         <header className={layoutStyles.header}>
-            <p>header</p>
+            <Link className={styles.logo} href="/">
+                <img className={styles.logoImg} src="/mu.svg" alt="logo" />
+                <div className={styles.logoText}>mumemo</div>
+            </Link>
         </header>
     )
 }
