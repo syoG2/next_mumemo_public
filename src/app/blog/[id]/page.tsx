f14d3a7ac01488934240ae24ceba99d97c859c8a
syoG2 <syo53594f@gmail.com>
2024-11-04 02:00:10 +0900

    デプロイ時のエラーの修正

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,12 +1,12 @@
 
 import { blogDatabaseId, getDatabase } from "@/components/notion/notion";
 
-// type Props = {
-//     params: { id: string }
-// };
+type Props = Readonly<{
+    params: { id: string };
+}>;
 
 // TODO: Articleコンポーネントの実装
-export default function Article({ params }: { params: { id: string } }) {
+export default function Article({ params }: Props) {
     return (
         <div>
             <h1>Article {params.id}</h1>
