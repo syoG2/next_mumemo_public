95e2a8983b70cfb5a5f1d0a8258489d9faca2c43
syoG2 <syo53594f@gmail.com>
2024-11-08 18:49:58 +0900

    blockToJsxの雛形を作成

--- src/components/notion/blockToJsx/blockToJsx.tsx
+++ src/components/notion/blockToJsx/blockToJsx.tsx
@@ -5,6 +5,10 @@ export function blockToJsx(block: ExPartialBlockObjectResponse | ExBlockObjectRe
         return <div>PartialBlockObjectResponse</div>
     } else {
         switch (block.object.type) {
+            case "bulleted_list":
+                return <h1>bulleted_list</h1>
+            case "numbered_list":
+                return <h1>numbered_list</h1>
             default:
                 return <div id={block.object.id}>{block.object.type ? `'${block.object.type}'には対応していません。` : `blockTypeが存在していません。`}</div>
         }
