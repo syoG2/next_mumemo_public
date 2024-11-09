b7a4aedb3d2c8b693ec4a6c89d4f6fa8302a5951
syoG2 <syo53594f@gmail.com>
2024-11-09 15:13:17 +0900

    apiへ送られるheaderの確認

--- src/app/api/viewcount/route.tsx
+++ src/app/api/viewcount/route.tsx
@@ -3,6 +3,7 @@ import { NextRequest, NextResponse } from 'next/server';
 export async function GET(request: NextRequest) {
     const referer = request.headers.get('referer');
     return NextResponse.json({
-        referer: referer
+        header: request.headers,
+        referer: referer,
     })
 }
