05c7fa80c90514bc23f37dbdfd126250094748d5
syoG2 <syo53594f@gmail.com>
2024-11-09 23:25:47 +0900

    react-katexを試す

--- src/components/notion/richText/richText.tsx
+++ src/components/notion/richText/richText.tsx
@@ -3,7 +3,9 @@ import type { RichTextItemResponse } from "@notionhq/client/build/src/api-endpoi
 import 'katex/dist/katex.min.css';
 import Link from 'next/link';
 import type { FC } from 'react';
-import Latex from 'react-latex-next';
+// TODO:react-katexが読み込めなかった件について調べる
+import { InlineMath } from 'react-katex';
+// import Latex from 'react-latex-next';
 import styles from './richText.module.css';
 
 type Props = {
@@ -34,8 +36,8 @@ export const RichText: FC<Props> = ({ text }) => {
                                 {textItem.plain_text}
                             </span>
                         } else if (textItem.type === 'equation') {
-                            // ret = <InlineMath key={index}>{textItem.plain_text}</InlineMath>
-                            ret = <Latex key={index}>${textItem.plain_text}$</Latex>
+                            ret = <InlineMath key={index}>{textItem.plain_text}</InlineMath>
+                            // ret = <Latex key={index}>${textItem.plain_text}$</Latex>
                         } else {
                             ret = <span key={index}></span>
                         }
