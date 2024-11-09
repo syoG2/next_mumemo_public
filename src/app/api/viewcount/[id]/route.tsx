b7c0c19b0581af18595b0a9b69ae7df81109a6dd
syoG2 <syo53594f@gmail.com>
2024-11-09 16:29:35 +0900

    viewcountのapiの修正

--- src/app/api/viewcount/[id]/route.tsx
+++ src/app/api/viewcount/[id]/route.tsx
@@ -2,7 +2,11 @@ import { incrementPageView } from '@/components/notion/notion';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
-    const { id } = params;
+    const { searchParams } = new URL(request.url);
+    const id = searchParams.get('id');
+    if (!id) {
+        return NextResponse.json({ error: 'ID is required' }, { status: 400 });
+    }
     await incrementPageView(id);
     return NextResponse.json({ id });
 }
