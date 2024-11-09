4a983e74a4b7b5d5f27cf35cd736f56b2914526c
syoG2 <syo53594f@gmail.com>
2024-11-09 16:31:49 +0900

    修正

--- src/app/api/viewcount/[id]/route.tsx
+++ src/app/api/viewcount/[id]/route.tsx
@@ -1,7 +1,7 @@
 import { incrementPageView } from '@/components/notion/notion';
 import { NextRequest, NextResponse } from 'next/server';
 
-export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
+export async function GET(request: NextRequest) {
     const { searchParams } = new URL(request.url);
     const id = searchParams.get('id');
     if (!id) {
