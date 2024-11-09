1abf1a8d781c04092164216d367b4101440a5e97
syoG2 <syo53594f@gmail.com>
2024-11-09 20:19:22 +0900

    headerのリンクの横幅を修正

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -86,6 +86,7 @@ export default async function Article({ params }: Props) {
                         {pageJson?.blocks.map((block) => {
                             return blockToJsx(block);
                         })}
+                        <Link href={`#header`}>一番上へ</Link>
                     </Main>
                     <ViewCounter pageId={props.id} />
                 </>
