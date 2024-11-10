2f1cc546c134ed04d029696ec981472ee997f620
syoG2 <syo53594f@gmail.com>
2024-11-10 14:46:46 +0900

    どうすればrevalidateに合わせてfetchできるのか

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,63 @@
 import { ArticleCard } from "@/components/articleCard/articleCard";
 import Main from "@/components/layout/main/main";
-import { blogDatabaseId, ExDatabaseObjectResponse, ExPageObjectResponse, ExPartialDatabaseObjectResponse, ExPartialPageObjectResponse, getDatabase } from "@/components/notion/notion";
+import { blogDatabaseId, getDatabase } from "@/components/notion/notion";
+import { unstable_cache } from "next/cache";
 import styles from "./page.module.css";
 
 // TODO: revalidateの使い方が正しいか調べる
 export const revalidate = 86400;
 
+const getTimeSortedPages = unstable_cache(
+  async () => {
+    const timeSortedParam = {
+      database_id: blogDatabaseId,
+      sorts: [
+        {
+          timestamp: "created_time" as const,
+          direction: "descending" as const,
+        }
+      ],
+      filter: {
+        property: "公開状態",
+        select: {
+          equals: "公開"
+        }
+      },
+    }
+    return await getDatabase(timeSortedParam);
+  },
+  [],
+  { revalidate: revalidate, tags: [] }
+)
+
+const getViewsSortedPages = unstable_cache(
+  async () => {
+    const viewsSortedParam = {
+      database_id: blogDatabaseId,
+      sorts: [
+        {
+          property: "閲覧数",
+          direction: "descending" as const,
+        }
+      ],
+      filter: {
+        property: "公開状態",
+        select: {
+          equals: "公開"
+        }
+      },
+    }
+    return await getDatabase(viewsSortedParam);
+  },
+  [],
+  { revalidate: revalidate, tags: [] }
+)
 
 // TODO:検索用のページを作る
-export default function Home({ timeSortedPages, viewsSortedPages }: { timeSortedPages: (ExPageObjectResponse | ExPartialPageObjectResponse | ExPartialDatabaseObjectResponse | ExDatabaseObjectResponse)[], viewsSortedPages: (ExPageObjectResponse | ExPartialPageObjectResponse | ExPartialDatabaseObjectResponse | ExDatabaseObjectResponse)[] }) {
+export default async function Home() {
+  const timeSortedPages = await getTimeSortedPages();
+  const viewsSortedPages = await getViewsSortedPages();
+
 
   return (
     <Main>
@@ -35,48 +84,4 @@ export default function Home({ timeSortedPages, viewsSortedPages }: { timeSorted
       </div>
     </Main>
   );
-}
-
-export const getStaticProps = async () => {
-  const timeSortedParam = {
-    database_id: blogDatabaseId,
-    sorts: [
-      {
-        timestamp: "created_time" as const,
-        direction: "descending" as const,
-      }
-    ],
-    filter: {
-      property: "公開状態",
-      select: {
-        equals: "公開"
-      }
-    },
-  }
-  const timeSortedPages = await getDatabase(timeSortedParam);
-
-  const viewsSortedParam = {
-    database_id: blogDatabaseId,
-    sorts: [
-      {
-        property: "閲覧数",
-        direction: "descending" as const,
-      }
-    ],
-    filter: {
-      property: "公開状態",
-      select: {
-        equals: "公開"
-      }
-    },
-  }
-  const viewsSortedPages = await getDatabase(viewsSortedParam);
-
-  return {
-    props: {
-      timeSortedPages,
-      viewsSortedPages
-    },
-    revalidate
-  }
 }
