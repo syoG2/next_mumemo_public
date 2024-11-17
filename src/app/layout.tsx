f4bc1aa65cacc2d1138f017028387c055b032899
syoG2 <syo53594f@gmail.com>
2024-11-17 17:10:12 +0900

    iconの設定

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,9 +34,9 @@ export const metadata: Metadata = {
     }
   },
   icons: {
-    icon: '/icon.svg',
-    shortcut: '/icon.svg',
-    apple: '/icon.svg',
+    icon: '/icon.png',
+    shortcut: '/icon.png',
+    apple: '/icon.png',
   }
 }
 
