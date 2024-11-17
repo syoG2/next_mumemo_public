bfea6034c60b5bffcfd0ba9196bd52b66f18bc95
syoG2 <syo53594f@gmail.com>
2024-11-17 15:40:34 +0900

    デバッグ用console.logを削除

--- src/app/blog/[id]/revalidate/route.tsx
+++ src/app/blog/[id]/revalidate/route.tsx
@@ -5,14 +5,11 @@ export async function GET(request: NextRequest) {
     const path = request.nextUrl.href;
     const REVALIDATE_SECRET = process.env.REVALIDATE_SECRET
     if (process.env.REVALIDATE_SECRET === request.headers.get("secret") && path) {
-        console.log(path)
         const pathParts = path.split("/").slice(3, -1);
         const newPath = "/" + pathParts.join("/");
         revalidatePath(newPath);
-        console.log(newPath)
         const basePath = "/";
         revalidatePath(basePath);
-        console.log(basePath)
         return NextResponse.json({
             revalidated: true,
             now: Date.now(),
