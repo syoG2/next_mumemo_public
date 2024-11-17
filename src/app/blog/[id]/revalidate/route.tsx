5deb9fc914240648256e24aaf48b2f27b3b7c963
syoG2 <syo53594f@gmail.com>
2024-11-17 15:00:11 +0900

    デバッグ

--- src/app/blog/[id]/revalidate/route.tsx
+++ src/app/blog/[id]/revalidate/route.tsx
@@ -4,16 +4,17 @@ import { NextRequest, NextResponse } from 'next/server';
 export async function GET(request: NextRequest) {
     const path = request.nextUrl.href
     const REVALIDATE_SECRET = process.env.REVALIDATE_SECRET
-    console.log(request.headers.get("secret"))
     if (process.env.REVALIDATE_SECRET === request.headers.get("secret") && path) {
         const pathParts = path.split("/");
         pathParts.pop();
         const newPath = pathParts.join("/");
         revalidatePath(newPath);
+        console.log(newPath)
         pathParts.pop();
         pathParts.pop();
         const basePath = pathParts.join("/");
         revalidatePath(basePath);
+        console.log(basePath)
         return NextResponse.json({
             revalidated: true,
             now: Date.now(),
