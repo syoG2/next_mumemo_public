4d83e2582ad75646fa57f08bfd640fa74f014a3c
syoG2 <syo53594f@gmail.com>
2025-01-12 00:57:12 +0900

    エラーの修正

--- src/components/notion/blocks/linkToPage/linkToPage.tsx
+++ src/components/notion/blocks/linkToPage/linkToPage.tsx
@@ -18,53 +18,55 @@ const fetcher = (url: string): Promise<any> => {
 }
 
 export const LinkToPage: FC<Props> = ({ block, nestBlocks }) => {
-    if (block.link_to_page.type !== 'page_id') {
+    const url = block.link_to_page.type === 'page_id'
+        ? `https://mumemo.vercel.app/blog/${block.link_to_page.page_id}`
+        : null;
+    const { data, error, isLoading } = useSWR<{ title: string, description: string, image: string }>(url ? path.join(`/api/bookmark?url=${url}`) : null, fetcher);
+    if (!url) {
         return <></>
-    } else {
-        const url = `https://mumemo.vercel.app/blog/${block.link_to_page.page_id}`
-        const { data, error, isLoading } = useSWR<{ title: string, description: string, image: string }>(path.join(`/api/bookmark?url=${url}`), fetcher);
-        if (isLoading && block.link_to_page.type === 'page_id') {
-
-            return (
-                <div id={block.id} className={styles.bookmark}>
-                    <Link className={styles.link} href={url} prefetch={true}>
-                        <div className={styles.linkText}>
-                            <div className={styles.url}>
-                                {url}
-                            </div>
-                        </div>
-                    </Link>
-                </div>
-            )
-        } else if (error) {
-            return (
-                <div id={block.id} className={styles.bookmark}>
-                    <Link className={styles.link} href={url} prefetch={true}>
-                        <div className={styles.linkText}>
-                            <div className={styles.url}>
-                                {url}
-                            </div>
-                        </div>
-                    </Link>
-                </div>
-            )
-        } else if (data) {
-            return (
-                <div id={block.id} className={styles.bookmark}>
-                    <Link className={styles.link} href={url} prefetch={true}>
-                        <div className={styles.linkText}>
-                            <div className={styles.title}>{data.title}</div>
-                            <div className={styles.description}>{data.description}</div>
-                            <div className={styles.url}>
-                                <img className={styles.favicon} src={`https://www.google.com/s2/favicons?domain=${url}`} alt="リンク先のfavicon" />
-                                {url}
-                            </div>
-                        </div>
-                        {data.image && <img className={styles.linkImage} src={data.image} alt="リンク先の画像" />}
-                    </Link>
-                </div>
-            )
-        }
-        return <div>エラー</div>
     }
+    if (isLoading && block.link_to_page.type === 'page_id') {
+
+        return (
+            <div id={block.id} className={styles.bookmark}>
+                <Link className={styles.link} href={url} prefetch={true}>
+                    <div className={styles.linkText}>
+                        <div className={styles.url}>
+                            {url}
+                        </div>
+                    </div>
+                </Link>
+            </div>
+        )
+    } else if (error) {
+        return (
+            <div id={block.id} className={styles.bookmark}>
+                <Link className={styles.link} href={url} prefetch={true}>
+                    <div className={styles.linkText}>
+                        <div className={styles.url}>
+                            {url}
+                        </div>
+                    </div>
+                </Link>
+            </div>
+        )
+    } else if (data) {
+        return (
+            <div id={block.id} className={styles.bookmark}>
+                <Link className={styles.link} href={url} prefetch={true}>
+                    <div className={styles.linkText}>
+                        <div className={styles.title}>{data.title}</div>
+                        <div className={styles.description}>{data.description}</div>
+                        <div className={styles.url}>
+                            <img className={styles.favicon} src={`https://www.google.com/s2/favicons?domain=${url}`} alt="リンク先のfavicon" />
+                            {url}
+                        </div>
+                    </div>
+                    {data.image && <img className={styles.linkImage} src={data.image} alt="リンク先の画像" />}
+                </Link>
+            </div>
+        )
+    }
+    return <div>エラー</div>
+
 };
